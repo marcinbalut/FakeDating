2821d71cb317259489409f8a5ff8dab2146a3e15
marcinbalut <marcinbalutpoland@gmail.com>
2023-09-19 17:20:24 +0200

    Implemented client side register functionality

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,15 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
   registerMode = false;
+  users: any;
+
+  constructor(private http: HttpClient, private accountService: AccountService) { }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
   }
 
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: users => this.users = users,
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed')      
+    });
+  }
+
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
+  }
 }
