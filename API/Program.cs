941b245b3ed313629ea5f1d08230eccbda949301
marcinbalut <marcinbalutpoland@gmail.com>
2023-09-21 15:49:31 +0200

    Added ExceptionMiddleware and its configuration

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using API.Errors;
 using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,8 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 app.UseCors(corsPolicyBuilder => corsPolicyBuilder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseHttpsRedirection();
