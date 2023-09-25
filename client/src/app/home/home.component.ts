05d5d77928ea7c8c63dfe521deade5529c29a4ca
marcinbalut <marcinbalutpoland@gmail.com>
2023-09-25 17:49:27 +0200

    Cleaned up home component

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AccountService } from '../_services/account.service';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -11,22 +9,13 @@ export class HomeComponent implements OnInit{
   registerMode = false;
   users: any;
 
-  constructor(private http: HttpClient, private accountService: AccountService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.getUsers();
   }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: users => this.users = users,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')      
-    });
   }
 
   cancelRegisterMode(event: boolean) {
