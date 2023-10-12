7b4115793be743400fb8f884e22ccde82cb62046
marcinbalut <marcinbalutpoland@gmail.com>
2023-10-12 15:44:06 +0200

    Added roles to seeding mechanism

--- API/Data/seed.cs
+++ API/Data/seed.cs
@@ -8,7 +8,7 @@ namespace API.Data;
 
 public class Seed
 {
-public static async Task SeedUsers(UserManager<AppUser> userManager)
+public static async Task SeedUsers(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
         {
             if (await userManager.Users.AnyAsync()) return;
 
@@ -18,13 +18,30 @@ public static async Task SeedUsers(UserManager<AppUser> userManager)
 
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData, options);
 
+            var roles = new List<AppRole> {
+                new AppRole{Name = "Member"},
+                new AppRole{Name = "Admin"},
+                new AppRole{Name = "Moderator"}
+            };
+
+            foreach (var role in roles)
+            {
+                await roleManager.CreateAsync(role);
+            }
+
             foreach (var user in users)
             {
-                using var hmac = new HMACSHA512();
-
                 user.UserName = user.UserName.ToLower();
-
                 await userManager.CreateAsync(user, "!234Pass");
+                await userManager.AddToRoleAsync(user, "Member");
             }
+
+            var admin = new AppUser
+            {
+                UserName = "admin"
+            };
+
+            await userManager.CreateAsync(admin, "!234Pass");
+            await userManager.AddToRolesAsync(admin, new[] {"Admin", "Moderator"});
         }
 }
