ba480d10db5ff29e3ae9dda54e149bac2209a889
marcinbalut <marcinbalutpoland@gmail.com>
2023-10-12 11:58:54 +0200

    Added to AppUser entity property UserRoles

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -20,4 +20,5 @@ public class AppUser : IdentityUser<int>
     public List<UserLike> LikedUsers { get; set; }
     public List<Message> MessagesSent { get; set; }
     public List<Message> MessagesReceived { get; set; }
+    public ICollection<AppUserRole> UserRoles { get; set; }
 }
