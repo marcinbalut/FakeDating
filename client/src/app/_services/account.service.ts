b1511b3cc1f9f9c74e8053e60732a51ed0d9e341
marcinbalut <marcinbalutpoland@gmail.com>
2023-10-13 17:05:52 +0200

    Added user roles to AccountService

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -32,10 +32,13 @@ export class AccountService {
           this.setCurrentUser(user);
         }
       })
-      );
-    }
-    
-    setCurrentUser(user: User) {
+    );
+  }
+
+  setCurrentUser(user: User) {
+    user.roles = [];
+    const roles = this.getDecodedToken(user.token).role;
+    Array.isArray(roles) ? user.roles = roles : user.roles.push(roles);
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
@@ -44,4 +47,9 @@ export class AccountService {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
   }
+
+  getDecodedToken(token: string) {
+    return JSON.parse(atob(token.split('.')[1]));
+  }
+
 }
